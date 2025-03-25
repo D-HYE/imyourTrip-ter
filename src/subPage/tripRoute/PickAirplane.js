71a937d5e997db14076f3b82d3891abcb666644d
hyok8 <hyo.k824@gmail.com>
2025-03-25 16:06:46 +0900

    항공권

--- src/subPage/tripRoute/PickAirplane.js
+++ src/subPage/tripRoute/PickAirplane.js
@@ -1,23 +1,68 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import useState
 
-const PickAirplane = ({ hyodata }) => {
-    useEffect(() => {
-        console.log(hyodata);
-    }, []);
 
-    const airplane = useParams();
-
+const PickAirplane = ({hyodata}) => {
+    
     return (
-        <div className="plane_list d-flex">
-            <div className="plane_left d-flex">
-                <div className="d-flex flex-column" id="airplaneArea">
-
+        <div className="plane_list">
+                <div className="plane_left d-flex">
+                    <ul className="d-flex flex-column" id="airplaneArea">
+                        <li className="d-flex">
+                            <div className="d-flex align-items-center gap-1">
+                                    <div className="planeLogo"></div>
+                                    <span></span>                            
+                            </div>
+                            <div className="plane_section_time d-flex gap-5">
+                                <div className="plane_time d-flex flex-column align-items-end gap-1">
+                                    <p>오전 06:25</p>
+                                    <p className="airport">인천(ICN)</p>
+                                </div>
+                                <div className="flight-time d-flex flex-column align-items-center gap-1">
+                                    <p>소요시간 n시간</p>
+                                    <div className ="arrow-right"></div>
+                                    <p>직항</p>
+                                </div>
+                                <div className="plane_time d-flex flex-column align-items-end gap-1">
+                                    <p>오전 10:10</p>
+                                    <p className="airport">가오슝(KHH)</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="d-flex">
+                            <div className="d-flex align-items-center gap-1">
+                                <div className="planeLogo"></div>
+                                <span></span>                            
+                            </div>
+                            <div className="plane_section_time d-flex gap-5">
+                                <div className="plane_time d-flex flex-column align-items-end gap-1">
+                                    <p>오전 10:10</p>
+                                    <p className="airport">가오슝(KHH)</p>
+                                </div>
+                                <div className="flight-time d-flex flex-column align-items-center gap-1">
+                                    <p>소요시간 n시간</p>
+                                    <div className ="arrow-right"></div>
+                                    <p>직항</p>
+                                </div>
+                                <div className="plane_time d-flex flex-column align-items-end gap-1">
+                                    <p>오전 06:25</p>
+                                    <p className="airport">인천(ICN)</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div>
+                        <ul>
+                            <li></li>
+                            <li><img src="" alt="찜"/></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="plane_right">
+                    <p>금액</p>
+                    <button><span>계획에 담기</span></button>
                 </div>
             </div>
-            <div className="plane_right">
-            </div>
-        </div>
     );
 };
 
--- src/subPage/tripRoute/PlanMaker.js
+++ src/subPage/tripRoute/PlanMaker.js
@@ -1,11 +1,91 @@
+import React from "react";
 import PickAirplane from "./PickAirplane";
-
-import hyodata from "../../data/hyodata.json"
+import hyodata from "../../data/hyodata.json"; // JSON 파일을 import로 가져오기
 
 export default function PlanMaker() {
     return (
-        <div>
-            <PickAirplane hyodata={hyodata}/>     
+        <div className="container">
+            <div className="plan_info d-flex align-items-center">
+                <ul className="d-flex order-1">
+                    <li><span>김효정곤듀예요</span>님의 일정</li>
+                </ul>
+                <ul className="d-flex gap-2 order-2">
+                    <li>인천 - 가오슝</li>
+                    <li>2025.02.18 - 2025.03.18</li>
+                    <li>인원 2명</li>
+                </ul> 
+                <ul className="plan_edit d-flex justify-content-end order-3">
+                    <li><a href="/">수정하기</a></li>
+                </ul>           
+            </div>
+            <div className="tab_content">
+                <ul className="d-flex gap-3">
+                    <li><a href="#none">#항공권</a></li>
+                    <li><a href="#none">#숙소</a></li>
+                    <li><a href="#none">#투어</a></li>
+                    <li><a href="#none">#티켓</a></li>
+                    <li><a href="#none">#랜드마크</a></li>
+                    <li><a href="#none">#교통패스</a></li>
+                    <li className="tripcar_tab"><a href="#none">#트립카</a></li>
+                </ul>
+            </div>
+            <div classNameName="plane_title d-flex justify-content-between align-items-center">
+                <ul className="d-flex align-items-center gap-3">
+                    <li className="plane_title_all">전체보기</li>
+                    <li>
+                        <div className="select_box">
+                            <div className="select_btn d-flex justify-content-between align-items-center ">
+                                <div className="selected_val">필터</div>
+                                <span className="arrow"></span>
+                            </div>
+                            <div className="option_list">
+                                <div className="filter_section d-flex flex-column gap-3">
+                                    <div className="d-flex flex-column gap-2">
+                                        <div className="filter_section_title">
+                                            <span className="title">경유</span>
+                                        </div>
+                                        <ul className="d-flex gap-3">
+                                            <li className="filter_radio d-flex align-items-center gap-1" >
+                                                <button type="button" className=""></button>
+                                                <span>직항</span>
+                                            </li>
+                                            <li className="filter_radio d-flex align-items-center gap-1" >
+                                                <button type="button" className=""></button>
+                                                <span>1회이상 경유</span>
+                                               
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="d-flex flex-column gap-2">
+                                        <div className="filter_section_title ">
+                                            <span className="title">시간대</span>
+                                        </div>
+                                        <div>
+                                            <p>가는 날</p>
+                                            <span className="time_range">00:00 ~ 24:00</span>
+                                            <div className="timeTable py-2">
+                                                <div id="sliderbar_go"></div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <p>오는 날</p>
+                                            <span className="time_range">00:00 ~ 24:00</span>
+                                            <div className="timeTable py-2">
+                                                <div  id="sliderbar_back"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>    
+                <ul className="d-flex gap-2">    
+                    <li className="before_btn"><a href="#none">이전</a></li>
+                    <li className="front_btn"><a href="#none">다음</a></li>
+                </ul>
+        </div>
+            <PickAirplane hyodata={hyodata} />
         </div>
     );
 }
