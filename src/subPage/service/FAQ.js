30d6044a29e8e2396f432ace8e519c2521055f0e
shin-eekk <shieek0525@gmail.com>
2025-05-30 09:54:57 +0900

    faq탭메뉴수정중

--- src/subPage/service/FAQ.js
+++ src/subPage/service/FAQ.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import faqdata from '../../data/faq.json';
+import ekmodule from '../../scss/faq.ek.module.scss';
 
 const FAQ = () => {
-    return (
-        <div>
-            
+    return (      
+        <div className={`${ekmodule.faq}`}>
+            <h2 className="">고객센터</h2>
+            <li>
+                <h4>자주 묻는 질문 FAQ </h4>
+                <button className={`${ekmodule.qnabtn}`}>1:1문의</button>
+            </li>
+            <ul className={`${ekmodule.faqtab}`}>
+                탭메뉴 컴포넌트
+            </ul>
+                <dl>
+                {faqdata.faq.map(
+                    (item) => {
+                        return(
+                            <>
+                            <dt><span className={`${ekmodule.qamark}`}>Q.</span>{item.question}
+                            <img src="https://d-hye.github.io/source/img/icon/polygon-bottom.svg"
+                            style={{ width: '15px', height: '15px', marginLeft: 'auto', marginRight:'10px'}}/>
+                            </dt>
+                            <dd>
+                            <span className={`${ekmodule.qamark}`}>A.</span>{item.answer}
+                            </dd>
+                            </>
+                        )
+                    }
+                )}
+                </dl>
         </div>
+        
     );
 };
 
